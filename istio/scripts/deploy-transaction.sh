f8bb394d0485e64363919aa79a70bed5e82b554a
Rafael Marins <rafa.marins@gmail.com>
2020-11-24 17:37:33 -0300

    fix database uses deployment instead dc

--- istio/scripts/deploy-transaction.sh
+++ istio/scripts/deploy-transaction.sh
@@ -15,7 +15,7 @@ echo Deploy Transaction service........
 oc project $USERXX-transaction || oc new-project $USERXX-transaction
 oc delete deployment,dc,bc,build,svc,route,pod,is --all
 
-echo "Waiting 30 seconds to finialize deletion of resources..."
+echo "Waiting 30 seconds to fininalize deletion of resources..."
 sleep 30
 
 oc new-app -e POSTGRESQL_USER=transaction \
@@ -25,10 +25,11 @@ oc new-app -e POSTGRESQL_USER=transaction \
 
 mvn clean package -DskipTests -f $CHE_PROJECTS_ROOT/fsi-workshop-v2m3-labs/transaction
 
-oc delete route transaction
+## uncomment if you don't want to expose the service
+# oc delete route transaction
 
-oc label dc/transaction-database app.openshift.io/runtime=postgresql --overwrite && \
+oc label deployment/transaction-database app.openshift.io/runtime=postgresql --overwrite && \
 oc label dc/transaction app.kubernetes.io/part-of=transaction --overwrite && \
-oc label dc/transaction-database app.kubernetes.io/part-of=transaction --overwrite && \
+oc label deployment/transaction-database app.kubernetes.io/part-of=transaction --overwrite && \
 oc annotate dc/transaction app.openshift.io/connects-to=transaction-database --overwrite && \
 oc annotate dc/transaction app.openshift.io/vcs-ref=ocp-4.5 --overwrite
